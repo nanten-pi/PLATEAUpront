532df85dcffbb5cdd348dfdc9384c96dfb430d7b
nanten_pi <88582003+nanten-pi@users.noreply.github.com>
2023-09-21 23:47:36 +0900

    main_Screen

--- mainapp/lib/main.dart
+++ mainapp/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mainapp/main_screen.dart';
 
 void main() {
   runApp(const MainApp());
