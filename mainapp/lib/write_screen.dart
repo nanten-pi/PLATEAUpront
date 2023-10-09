3a5951f369a30a2ccee2005007d505577850672e
nanten_pi <88582003+nanten-pi@users.noreply.github.com>
2023-10-09 20:32:03 +0900

    writingはhtmlを読み込んで3dを表示

--- mainapp/lib/write_screen.dart
+++ mainapp/lib/write_screen.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class writeingPage extends StatelessWidget {
   const writeingPage({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('debugwritewindow'),
-      ),
-      body: Container(
-        color: Colors.red,
+      body: WebViewWidget(
+        controller: WebViewController()
+          ..setJavaScriptMode(JavaScriptMode.unrestricted)
+          ..loadRequest(Uri.parse("http://localhost:8080/")),
       ),
     );
   }
