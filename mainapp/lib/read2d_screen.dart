7bede29b1dd257b2070916acf9b43d493dd167d7
nanten_pi <88582003+nanten-pi@users.noreply.github.com>
2023-10-09 16:49:58 +0900

    エラーも多いけどとりあえず修正完了+webviewのクラスエラーを取り除けた

--- mainapp/lib/read2d_screen.dart
+++ mainapp/lib/read2d_screen.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class MapPage extends StatelessWidget {
   const MapPage({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('maptestpage'),
-      ),
-      body: Container(
-        color: Colors.red,
+      body: WebViewWidget(
+        controller: WebViewController()
+          ..loadRequest(Uri.parse("https://www.google.com/maps/")),
       ),
     );
   }
