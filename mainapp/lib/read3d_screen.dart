fee15e395fe99d7de352034e0f1bde1fed2a0ffd
ragilispec <godof256@gmail.com>
2023-10-18 00:35:43 +0900

    自環境でいいかんじにかけたやつをひとまずコピペ

--- mainapp/lib/read3d_screen.dart
+++ mainapp/lib/read3d_screen.dart
@@ -11,7 +11,7 @@ class web3DviewPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('Clocky3dViewer'),
+          title: const Text('Clocky3dVieweraaaaa'),
         ),
         body: const ModelViewer(
           //assetsでも可能
@@ -19,6 +19,74 @@ class web3DviewPage extends StatelessWidget {
           autoRotate: true,
           cameraControls: true,
         ));
+  }
+}
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool switch1 = false;
+  bool switch2 = false;
+  bool switch3 = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          actions: <Widget>[
+            PopupMenuButton<String>(
+              onSelected: (String result) {},
+              itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+                PopupMenuItem<String>(
+                  value: 'Switch 1',
+                  child: SwitchListTile(
+                    title: const Text('Switch 1'),
+                    value: switch1,
+                    onChanged: (bool value) {
+                      setState(() {
+                        switch1 = value;
+                      });
+                    },
+                  ),
+                ),
+                PopupMenuItem<String>(
+                  value: 'Switch 2',
+                  child: SwitchListTile(
+                    title: const Text('Switch 2'),
+                    value: switch2,
+                    onChanged: (bool value) {
+                      setState(() {
+                        switch2 = value;
+                      });
+                    },
+                  ),
+                ),
+                PopupMenuItem<String>(
+                  value: 'Switch 3',
+                  child: SwitchListTile(
+                    title: const Text('Switch 3'),
+                    value: switch3,
+                    onChanged: (bool value) {
+                      setState(() {
+                        switch3 = value;
+                      });
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
 //コード整形便利すぎる
